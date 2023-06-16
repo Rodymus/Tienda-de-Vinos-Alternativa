acc4cd141dd862092628c2e867e4cee4a9026e3e
Rodymus <61990609+Rodymus@users.noreply.github.com>
2023-06-16 16:20:03 -0300

    1 file modified, 1 added

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+  return (
+    <div>
+      <h2>Bienvenido al carrito</h2>
+    </div>
+  )
+}
+
+export default Cart
--- src/components/cart/CartWidget.jsx
+++ src/components/cart/CartWidget.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
   return (
     <div>
+      <Link to={"/Cart"}>
       <span class="material-symbols-outlined">shopping_cart</span>
+      </Link>
 <span>5</span>
     </div>
   )
