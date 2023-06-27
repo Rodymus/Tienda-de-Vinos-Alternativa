0e8a11d45b871ab629bbb3a0a1b666cf77fcdbe9
Rodymus <61990609+Rodymus@users.noreply.github.com>
2023-06-27 11:48:43 -0300

    filtrado completado en firebase

--- src/components/items/ItemListContainer.jsx
+++ src/components/items/ItemListContainer.jsx
@@ -1,41 +1,52 @@
 import ItemList from './ItemList';
+import { db } from '../../firebaseConfig';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-import { products } from '../../productsMock';
 import { useParams } from 'react-router-dom';
-import { PacmanLoader } from "react-spinners";
+import { PacmanLoader } from 'react-spinners';
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
-
   const { categoryN } = useParams();
 
   useEffect(() => {
-    let productosFiltrados = products.filter((products) => products.category === categoryN);
 
-    const tarea = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(categoryN ? productosFiltrados : products);
-      }, 2000);
-    });
+    let itemsCollection = collection(db, "products")
 
-    tarea
-      .then((respuesta) => setItems(respuesta))
-      .catch((rechazo) => {
-        console.log(rechazo);
-      });
+    let consulta;
+    
+    if(categoryN){
+      consulta = query(itemsCollection, where("category", "==", categoryN))
+    }else{
+      consulta = itemsCollection
+    }
+
+    getDocs(consulta)
+      .then((res) => {
+        let products = res.docs.map((elemento) => {
+          return {
+            ...elemento.data(),
+            id: elemento.id,
+          };
+        });
+        setItems(products);
+      })
+      .catch((err) => console.log(err));
+    
   }, [categoryN]);
 
   //if con return temprano - TECNICA DE RENDERING
   if (items.length === 0) {
     return (
-      <center><h1>
-        <PacmanLoader color="#FFFF00" />
-      </h1></center>
+      <center>
+        <h1>
+          <PacmanLoader color="#FFFF00" />
+        </h1>
+      </center>
     );
   }
 
-  return <ItemList items={items} />
-  
+  return <ItemList items={items} />;
 };
 
 export default ItemListContainer;
